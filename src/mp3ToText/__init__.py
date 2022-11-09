90325c1ff52c10135bb8545c265952c9e5e39766
matt manes <113395539+matt-manes@users.noreply.github.com>
2022-11-09 16:36:46 -0600

    Add files via upload

---
+++ src/mp3ToText/__init__.py
@@ -0,0 +1,3 @@
+""" FFMPEG needs to be installed and in your PATH for this to work. """
+from .mp3ToText import getText, cleanUp
+cleanUp()
---
+++ src/mp3ToText/mp3ToText.py
@@ -0,0 +1,57 @@
+import speech_recognition
+from pydub import AudioSegment
+import requests
+from whosYourAgent import getAgent
+from datetime import datetime
+from pathlib import Path
+import os
+root = Path(__file__).parent
+
+""" Extract text from an mp3 url. """
+
+def downloadMp3File(url:str)->Path:
+    """ Downloads an mp3 file to
+    a folder named audio in
+    the same folder as this file.\n
+    Returns a Path object for the
+    saved file."""
+    dest = root/'audio'
+    dest.mkdir(parents=True, exist_ok=True)
+    mp3Path = (dest/datetime.now().timestamp()).with_suffix('.mp3')
+    source = requests.get(url, headers={'User-Agent': getAgent()})
+    with mp3Path.open('wb') as file:
+        file.write(source.content)
+    return mp3Path
+
+def convertMp3ToWav(mp3Path:Path)->Path:
+    """ Converts an mp3 file to a wav file
+    of the same name, deletes the mp3 file, 
+    and returns a Path object for the wav file."""
+    audio = AudioSegment.from_mp3(mp3Path)
+    wavPath = mp3Path.with_suffix('.wav')
+    audio.export(wavPath, format='wav')
+    mp3Path.unlink()
+    return wavPath
+
+def extractTextFromWav(wavPath:Path)->str|None:
+    """ Extracts and returns text from wav file. """
+    recognizer = speech_recognition.Recognizer()
+    with speech_recognition.AudioFile(str(wavPath)) as source:
+        audio = recognizer.record(source)
+        text = recognizer.recognize_google(audio)
+    wavPath.unlink()
+    return text
+
+def getText(url:str)->str:
+    """ Returns text from an mp3 file
+    located at the given url."""
+    return extractTextFromWav(convertMp3ToWav(downloadMp3File(url)))
+
+def cleanUp():
+    """ Removes any files from the audio directory
+    older than 5 minutes."""
+    audioDir = (root/'audio')
+    if audioDir.exists():
+        for file in (root/'audio').glob('*.*'):
+           if (datetime.now().timestamp() - os.stat(file).st_ctime) > (60 * 5):
+               file.unlink()
