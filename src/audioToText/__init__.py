90238c2a44f534b1509e9cc7ff3260d3d8365d95
matt manes <113395539+matt-manes@users.noreply.github.com>
2023-01-02 13:56:37 -0600

    Delete src/audioToText directory

--- src/audioToText/__init__.py
+++
@@ -1,5 +0,0 @@
-from .audioToText import (cleanUp, getTextFromMp3, getTextFromUrl,
-                          getTextFromWav)
-
-""" FFMPEG needs to be installed and in your PATH for this to work. """
-cleanUp()
